866af7ca257b0d8e1f6c8ea0dd7dbf179b9e6d55
JennyTromTheBlock <114472178+JennyTromTheBlock@users.noreply.github.com>
2023-11-12 17:44:22 +0100

    mock data

--- backend/infrastructure/repository/UserReposity.cs
+++ backend/infrastructure/repository/UserReposity.cs
@@ -6,6 +6,18 @@ public class UserRepository
 {
     public User Create(string email, string fullName, string phoneNumber, string profileUrl)
     {
-        throw new NotImplementedException("create in user respository is not implemented yet ");
+        //throw new NotImplementedException("create in user respository is not implemented yet ");
+        var user = new User
+        {
+            Email = email,
+            FullName = fullName,
+            PhoneNumber = phoneNumber,
+            Created = DateTime.Now,
+            ProfileUrl = profileUrl
+        };
+
+        Console.Write("You created a user");
+        Console.Write("email:  " + user.Email);
+        return user;
     }
 }
