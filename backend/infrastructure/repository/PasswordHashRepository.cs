fbd3b291ed6e8a1b96225a9f39bf9fd3a00c77d5
JennyTromTheBlock <114472178+JennyTromTheBlock@users.noreply.github.com>
2023-11-12 21:53:15 +0100

    ,k

--- backend/infrastructure/repository/PasswordHashRepository.cs
+++ backend/infrastructure/repository/PasswordHashRepository.cs
@@ -1,13 +1,17 @@
-﻿using infrastructure.dataModels;
+﻿using Dapper;
+using infrastructure.dataModels;
+using Npgsql;
 
 namespace infrastructure.repository;
 
 
 public class PasswordHashRepository
 {
+    private readonly NpgsqlDataSource _dataSource;
 
-    public PasswordHashRepository()
+    public PasswordHashRepository(NpgsqlDataSource dataSource)
     {
+        _dataSource = dataSource;
     }
 
     public PasswordHash GetByEmail(string email)
@@ -15,9 +19,27 @@ public class PasswordHashRepository
         throw new NotImplementedException();
     }
     
+
     public void Create(string userId, string hash, string salt, string algorithm)
     {
-        throw new NotImplementedException();
+        // Define the SQL query to insert a new password hash
+        string sql = "INSERT INTO password_hash (user_email, hash, salt, algorithm) " + 
+                     "VALUES (@userId, @hash, @salt, @algorithm)";
+
+            // Create an object with the provided data
+        var passwordHash = new
+        { 
+            userId = userId, 
+            hash = hash, 
+            salt = salt, 
+            algorithm = algorithm
+            };
+
+        using (var conn = _dataSource.OpenConnection())
+        {
+            // Execute the SQL query using Dapper
+            conn.Execute(sql, passwordHash);
+        }
     }
 
     public void Update(string userId, string hash, string salt, string algorithm)
--- backend/infrastructure/repository/UserReposity.cs
+++ backend/infrastructure/repository/UserReposity.cs
@@ -1,17 +1,39 @@
-﻿using infrastructure.dataModels;
+﻿using Dapper;
+using infrastructure.dataModels;
+using Npgsql;
+
 
 namespace infrastructure.repository;
 
 public class UserRepository
 {
+    private readonly NpgsqlDataSource _dataSource;
 
-    public UserRepository()
+    public UserRepository(NpgsqlDataSource dataSource)
     {
+        _dataSource = dataSource;
     }
+
 
     public User Create(string fullName, string email, string phone, string profileUrl)
     {
-        throw new NotImplementedException();
+        var sql = $@"
+            INSERT INTO users.user (Email, FullName, PhoneNumber, Created, ProfileUrl) " +
+                  "VALUES (@Email, @FullName, @PhoneNumber, @Created, @ProfileUrl) " +
+                  "RETURNING Email, FullName, PhoneNumber, Created, ProfileUrl";
+
+        var user = new User
+        {
+            Email = email,
+            FullName = fullName,
+            PhoneNumber = phone,
+            Created = DateTime.Now,
+            ProfileUrl = profileUrl
+        };
+        using (var conn = _dataSource.OpenConnection())
+        {
+            return conn.QueryFirst<User>(sql, new {user});
+        }
     }
 
     public User? GetById(string id)
