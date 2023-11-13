7cab33fcebc1c61ad6e2e652dfc0a03a0f07977e
JennyTromTheBlock <114472178+JennyTromTheBlock@users.noreply.github.com>
2023-11-13 12:25:27 +0100

    front end intercept

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -12,7 +12,8 @@ import {HTTP_INTERCEPTORS} from "@angular/common/http";
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: HTTP_INTERCEPTORS, useClass: ErrorHttpInterceptor, multi: true },
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorHttpInterceptor, multi: true },
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- frontend/src/interceptors/error-http-interceptors.ts
+++ frontend/src/interceptors/error-http-interceptors.ts
@@ -1,14 +1,34 @@
 import { Injectable } from '@angular/core';
 import {
-  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest
+  HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse
 } from '@angular/common/http';
 
-import { Observable } from 'rxjs';
+import {catchError, Observable} from 'rxjs';
+import {ToastController} from "@ionic/angular";
+
+
 
 @Injectable()
 export class ErrorHttpInterceptor implements HttpInterceptor {
+
   intercept(req: HttpRequest<any>, next: HttpHandler):
     Observable<HttpEvent<any>> {
-    return next.handle(req);
+    return next.handle(req).pipe(catchError(async e => {
+      if (e instanceof HttpErrorResponse) {
+        this.showError(e.statusText);
+      }
+      throw e;
+    }));
+  }
+  constructor(private readonly toast: ToastController) {}
+
+  private async showError(message: string) {
+    return (await this.toast.create({
+      message: message,
+      duration: 5000,
+      color: 'danger'
+    })).present()
   }
 }
+
+
