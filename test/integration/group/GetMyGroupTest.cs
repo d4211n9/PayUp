aa4b765bca3113fd402668ab3f7c71d7a1078d64
steffaneckeroth <114472157+steffaneckeroth@users.noreply.github.com>
2023-11-23 09:22:38 +0100

    sat lidt op til test

---
+++ test/integration/group/GetMyGroupTest.cs
@@ -0,0 +1,16 @@
+﻿namespace test.integration.group;
+
+public class GetGroupTest
+{
+    private HttpClient _httpClient;
+
+    [SetUp]
+    public void Setup()
+    {
+        _httpClient = new HttpClient();
+        Helper.TriggerRebuild();
+    }
+    
+    
+    
+}
