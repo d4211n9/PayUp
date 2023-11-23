0d8f06813f0433622eb0a16ab6bf395e6619b49b
micdra01 <114472076+micdra01@users.noreply.github.com>
2023-11-23 10:48:38 +0100

    GetMyGroups test

---
+++ test/Models.cs
@@ -0,0 +1,13 @@
+﻿namespace test;
+
+public class Models
+{
+    public class Group
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; }
+        public required string Image_Url { get; set; }
+        public DateTime Created_Date { get; set; }
+    }
+}
