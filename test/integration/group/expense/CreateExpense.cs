d2628654cb4efa9f98515cc48666509237f1412f
steffaneckeroth <114472157+steffaneckeroth@users.noreply.github.com>
2023-11-30 13:28:37 +0100

    testerlige

--- test/integration/group/expense/CreateExpense.cs
+++ test/integration/group/expense/CreateExpense.cs
@@ -1,4 +1,8 @@
-﻿namespace test.integration.group;
+﻿using System.Net.Http.Json;
+using FluentAssertions;
+using FluentAssertions.Execution;
+
+namespace test.integration.group;
 
 public class CreateExpense
 {
@@ -10,12 +14,56 @@ public class CreateExpense
         _httpClient = new HttpClient();
         Helper.TriggerRebuild();
     }
+
+    [TestCase (1, 1, "ValidBajere", 150, "2023-11-24T15:58:41.045Z", TestName = "Valid")]
+    [TestCase (1, 1, "ValidBajere", 0, "2023-11-24T15:58:41.045Z", TestName = "InvalidAmount")] 
     
+    public async Task Create(
+        int userId,
+        int groupId,
+        string description,
+        int amount,
+        string createdDate
+    )
     
-    
-    
-    
-    
-    
-    
+    {
+        var expense = new
+        {
+            UserId = userId,
+            GroupId = groupId,
+            Description = description,
+            Amount = amount,
+            CreatedDate = createdDate
+        };
+        
+        string url = "http://localhost:5100/api/groups/1/create";
+        HttpResponseMessage response;
+
+        try
+        {
+            _httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+            response = await _httpClient.PostAsJsonAsync(url, expense);
+            TestContext.WriteLine("The full body response: "
+                                  + await response.Content.ReadAsStringAsync());
+        }
+        catch (Exception e)
+        {
+            throw new Exception(Helper.NoResponseMessage, e);
+        }
+        
+        using (new AssertionScope())
+        {
+            string testName = TestContext.CurrentContext.Test.Name;
+
+            switch (testName)
+            {
+                case "Valid":
+                    response.IsSuccessStatusCode.Should().BeTrue();
+                    break;
+                default:
+                    response.IsSuccessStatusCode.Should().BeFalse();
+                    break;
+            }
+        }
+    }
 }
