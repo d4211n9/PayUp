5a30093dfb64a19363e638d0d3841cf0285c2c14
steffaneckeroth <114472157+steffaneckeroth@users.noreply.github.com>
2023-12-07 13:50:53 +0100

    dublicate import

--- frontend/src/app/expense/expense.module.ts
+++ frontend/src/app/expense/expense.module.ts
@@ -1,13 +1,10 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {ExpensecardComponent} from "./expensecard/expensecard.component";
 import {IonicModule} from "@ionic/angular";
 import {CreateexpenseComponent} from "./createexpense/createexpense.component";
-import {ReactiveFormsModule} from "@angular/forms";
-import {SettleComponent} from "./settle/settle.component";
-import {GroupModule} from "../group/group.module";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-
+import {SettleComponent} from "./settle/settle.component";
 
 
 @NgModule({
